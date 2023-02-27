ac101e62f3e3cd815dfd9f13d46424a5a347b767
Kevin Wang <kevin@xorkevin.com>
2023-02-27 00:11:52 -0800

    Remove default slog time and source keys

--- slog_handler.go
+++ slog_handler.go
@@ -3,14 +3,13 @@ package klog
 import (
 	"context"
 	"io"
+	"time"
 
 	"golang.org/x/exp/slog"
 )
 
 var slogBuiltinKeys = map[string]struct{}{
 	slog.LevelKey:   {},
-	slog.TimeKey:    {},
-	slog.SourceKey:  {},
 	slog.MessageKey: {},
 }
 
@@ -20,10 +19,9 @@ type (
 		FieldTimeInfo string
 		FieldCaller   string
 		FieldPath     string
-		FieldMsg      string
 		PathSeparator string
 		Path          string
-		fieldsSet     map[string]struct{}
+		attrKeySet    map[string]struct{}
 		slogHandler   slog.Handler
 	}
 )
@@ -31,13 +29,12 @@ type (
 // NewSlogHandler creates a new [*SlogHandler]
 func NewSlogHandler(handler slog.Handler) *SlogHandler {
 	return &SlogHandler{
-		FieldTimeInfo: "ti",
+		FieldTimeInfo: "t",
 		FieldCaller:   "caller",
 		FieldPath:     "path",
-		FieldMsg:      "msg",
 		PathSeparator: ".",
 		Path:          "",
-		fieldsSet:     map[string]struct{}{},
+		attrKeySet:    map[string]struct{}{},
 		slogHandler:   handler,
 	}
 }
@@ -50,7 +47,7 @@ func NewJSONSlogHandler(w io.Writer) *SlogHandler {
 	)
 }
 
-func copyFieldsSet(s map[string]struct{}) map[string]struct{} {
+func copyStringSet(s map[string]struct{}) map[string]struct{} {
 	m := map[string]struct{}{}
 	for k := range s {
 		m[k] = struct{}{}
@@ -61,23 +58,35 @@ func copyFieldsSet(s map[string]struct{}) map[string]struct{} {
 func (h *SlogHandler) clone() *SlogHandler {
 	return &SlogHandler{
 		FieldTimeInfo: h.FieldTimeInfo,
-		FieldCaller:   h.FieldCaller,
 		FieldPath:     h.FieldPath,
-		FieldMsg:      h.FieldMsg,
 		PathSeparator: h.PathSeparator,
 		Path:          h.Path,
-		fieldsSet:     copyFieldsSet(h.fieldsSet),
+		attrKeySet:    copyStringSet(h.attrKeySet),
 		slogHandler:   h.slogHandler,
 	}
+}
+
+func (h *SlogHandler) checkAttrKey(k string) bool {
+	if k == h.FieldTimeInfo || k == h.FieldCaller || k == h.FieldPath {
+		return true
+	}
+	if _, ok := slogBuiltinKeys[k]; ok {
+		return true
+	}
+	if _, ok := h.attrKeySet[k]; ok {
+		return true
+	}
+	return false
 }
 
 func (h *SlogHandler) Enabled(ctx context.Context, level slog.Level) bool {
 	return h.slogHandler.Enabled(ctx, level)
 }
 
-func (h *SlogHandler) Handle(ctx context.Context, rec slog.Record) {
+func (h *SlogHandler) Handle(ctx context.Context, r slog.Record) {
+	r2 := slog.NewRecord(time.Time{}, r.Level, r.Message, 0)
 	// TODO add attrs
-	h.slogHandler.Handle(ctx, rec)
+	h.slogHandler.Handle(ctx, r2)
 }
 
 func (h *SlogHandler) Subhandler(pathSegment string, attrs []slog.Attr) Handler {
@@ -88,13 +97,10 @@ func (h *SlogHandler) Subhandler(pathSegment string, attrs []slog.Attr) Handler 
 	if len(attrs) > 0 {
 		attrsToAdd := make([]slog.Attr, 0, len(attrs))
 		for _, i := range attrs {
-			if _, ok := slogBuiltinKeys[i.Key]; ok {
+			if h2.checkAttrKey(i.Key) {
 				continue
 			}
-			if _, ok := h2.fieldsSet[i.Key]; ok {
-				continue
-			}
-			h2.fieldsSet[i.Key] = struct{}{}
+			h2.attrKeySet[i.Key] = struct{}{}
 			attrsToAdd = append(attrsToAdd, i)
 		}
 		if len(attrsToAdd) > 0 {
